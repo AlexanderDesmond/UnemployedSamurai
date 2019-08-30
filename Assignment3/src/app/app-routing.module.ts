a115bb0a417f47d57c7710f57abde4706b91686a
“Alex” <AlexanderDesmond848898@gmail.com>
2019-08-30 18:54:14 +1000

    Refactored components for login and registration pages. Also added Angular Material.

--- Assignment3/src/app/app-routing.module.ts
+++ Assignment3/src/app/app-routing.module.ts
@@ -1,11 +1,17 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { ContentComponent } from "./content/content.component";
+import { LoginFormComponent } from "./modules/login/login-form/login-form.component";
+import { RegisterFormComponent } from "./modules/register/register-form/register-form.component";
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", component: ContentComponent },
+  { path: "login", component: LoginFormComponent },
+  { path: "register", component: RegisterFormComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- Assignment3/src/app/app.module.ts
+++ Assignment3/src/app/app.module.ts
@@ -1,15 +1,21 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { ContentComponent } from './content/content.component';
-import { PopularDiscussionsComponent } from './popular-discussions/popular-discussions.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
-import { LeaderboardComponent } from './leaderboard/leaderboard.component';
-import { DiscussionComponent } from './discussion/discussion.component';
-import { LeaderboardItemComponent } from './leaderboard-item/leaderboard-item.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { ContentComponent } from "./content/content.component";
+import { PopularDiscussionsComponent } from "./popular-discussions/popular-discussions.component";
+import { HeaderComponent } from "./shared/header/components/header-container/header.component";
+import { FooterComponent } from "./shared/footer/components/footer-container/footer.component";
+import { LeaderboardComponent } from "./leaderboard/leaderboard.component";
+import { DiscussionComponent } from "./discussion/discussion.component";
+import { LeaderboardItemComponent } from "./leaderboard-item/leaderboard-item.component";
+import { HomeModule } from "./modules/home/home.module";
+import { LoginModule } from "./modules/login/login.module";
+import { RegisterModule } from "./modules/register/register.module";
+import { HeaderModule } from "./shared/header/header.module";
+import { FooterModule } from "./shared/footer/footer.module";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 @NgModule({
   declarations: [
@@ -24,9 +30,15 @@ import { LeaderboardItemComponent } from './leaderboard-item/leaderboard-item.co
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HomeModule,
+    LoginModule,
+    RegisterModule,
+    HeaderModule,
+    FooterModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
