f2ee1f64206182a6ed0eaf2a856dd0f580f792c7
Alexander Desmond <alex@Alexanders-MacBook-Pro.local>
2019-08-31 15:15:24 +1000

    Refactored some components on home page.

--- Assignment3/src/app/app-routing.module.ts
+++ Assignment3/src/app/app-routing.module.ts
@@ -3,9 +3,10 @@ import { Routes, RouterModule } from "@angular/router";
 import { ContentComponent } from "./content/content.component";
 import { LoginFormComponent } from "./modules/login/login-form/login-form.component";
 import { RegisterFormComponent } from "./modules/register/register-form/register-form.component";
+import { HomeContainerComponent } from "./modules/home/home-container/home-container.component";
 
 const routes: Routes = [
-  { path: "", component: ContentComponent },
+  { path: "", component: HomeContainerComponent },
   { path: "login", component: LoginFormComponent },
   { path: "register", component: RegisterFormComponent }
 ];
