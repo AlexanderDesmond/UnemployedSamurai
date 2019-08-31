c7a97d88eff603b251d7d3cebca5499b892de62f
Alexander Desmond <alex@Alexanders-MacBook-Pro.local>
2019-08-31 21:27:00 +1000

    Removed outdated components.

--- Assignment3/src/app/app-routing.module.ts
+++ Assignment3/src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
-import { ContentComponent } from "./content/content.component";
 import { LoginFormComponent } from "./modules/login/login-form/login-form.component";
 import { RegisterFormComponent } from "./modules/register/register-form/register-form.component";
 import { HomeContainerComponent } from "./modules/home/home-container/home-container.component";
