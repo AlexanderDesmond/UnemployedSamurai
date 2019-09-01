bc9259353a259f794ad245e27bd1cad6126c3e84
jaspreetpanesar <jaspreetpanesar16@gmail.com>
2019-09-01 14:19:57 +1000

    api initial commit

---
+++ backend-api/server.js
@@ -0,0 +1,32 @@
+var express = require('express'),
+    app = express(),
+    port = process.env.PORT || 3000,
+    mongoose = require('mongoose'),
+    User = require('./api/models/apiModel'),
+    bodyParser = require('body-parser');
+
+
+// moongose instance
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/userdb');
+
+
+app.use(bodyParser.urlencoded( {extended: true} ));
+app.use(bodyParser.json());
+
+// register routes
+var routes = require('./api/routes/apiRoutes');
+routes(app); 
+
+app.listen(port);
+console.log("Backend API start on port: " + port);
+
+
+
+
+
+
+
+
+
+
