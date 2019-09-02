faec4930425d5879ba754b6a9cc3d61936ac55af
Alexander Desmond <alex@Alexanders-MacBook-Pro.local>
2019-09-02 18:42:37 +1000

    Replaced confirm password field with toggle to unmask password.

--- Assignment3/src/app/modules/register/register.module.ts
+++ Assignment3/src/app/modules/register/register.module.ts
@@ -4,8 +4,10 @@ import { RegisterFormComponent } from "./register-form/register-form.component";
 
 import { MaterialModule } from "../../material.module";
 
+import { MatPasswordStrengthModule } from "@angular-material-extensions/password-strength";
+
 @NgModule({
   declarations: [RegisterFormComponent],
-  imports: [CommonModule, MaterialModule]
+  imports: [CommonModule, MaterialModule, MatPasswordStrengthModule]
 })
 export class RegisterModule {}
