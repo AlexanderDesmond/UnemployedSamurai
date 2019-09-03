e37cab2730fdbf3d8dfeb5add15b7790303a1a06
jaspreetpanesar <jaspreetpanesar16@gmail.com>
2019-09-03 22:34:56 +1000

    validate username uniqueness on create new user call

--- backend-api/api/controllers/apiController.js
+++ backend-api/api/controllers/apiController.js
@@ -13,13 +13,37 @@ exports.list_all_users = function(req, res) {
 
 
 exports.create_user = function(req, res) {
-    var new_user = new User(req.body);
-    new_user.save(function(err, user) {
-        if (err)
-            res.send(err);
-        res.json(user);
-    })
 
+    // query db for username
+    User.find({username: req.body.username},
+        function (err, results) {
+            if (err)
+                res.send(err);
+
+            // found username?
+            if (!results.length) {
+                // create new user
+                var new_user = new User(req.body);
+
+                // save user to database and send response
+                new_user.save(function(err, user) {
+                    if (err)
+                        res.send(err);
+                    res.json(user);
+                })
+            } 
+            // send 409 conflict if username exists
+            else {
+                res.send(409, "username already exists");
+            }
+        }
+        
+        );
+    
+
+
+
+    
 
 };
 
