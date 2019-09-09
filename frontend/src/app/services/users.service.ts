139e27d067fb64044ab5499e2f553643ed2c8539
“Alex” <AlexanderDesmond848898@gmail.com>
2019-09-09 21:48:45 +1000

    Added posts service.

--- frontend/src/app/services/users.service.ts
+++ frontend/src/app/services/users.service.ts
@@ -21,29 +21,6 @@ import { User } from "../user.interface";
 export class UsersService {
   constructor(private http: HttpClient) {}
 
-  // getUsers(): Observable<User[]> {
-  //   return this.http.get<User[]>("http://localhost:3000/users/");
-  // }
-
-  // getUser(username: String): Observable<User> {
-  //   return this.http.get<User>("http://localhost:3000/users/" + username);
-  // }
-
-  createUser(user: User): Observable<User> {
-    return this.http.post<User>("http://localhost:3000/users/", user);
-  }
-
-  updateUser(user: User): Observable<void> {
-    return this.http.put<void>(
-      "http://localhost3000/users/" + user.username,
-      user
-    );
-  }
-
-  deleteUser(username: string) {
-    return this.http.delete("http://localhost3000/users/" + username);
-  }
-
   // This is implemented in the frontend!
   loginUser(username: string, password: string) {
     return this.http.post("/api/login/", { username, password });
