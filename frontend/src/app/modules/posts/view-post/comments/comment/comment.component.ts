32f0cec01bf0143a0de9afabd080072ce9b903f0
jaspreetpanesar <jaspreetpanesar16@gmail.com>
2019-10-02 13:56:05 +1000

    allow commenting to comments

--- frontend/src/app/modules/posts/view-post/comments/comment/comment.component.ts
+++ frontend/src/app/modules/posts/view-post/comments/comment/comment.component.ts
@@ -1,20 +1,30 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Post } from "src/app/post.interface";
 import { PostsService } from "../../../../../services/posts.service";
+import { AuthenticationService } from "../../../../../services/authentication.service";
 
 @Component({
   selector: 'app-comment',
   templateUrl: './comment.component.html',
   styleUrls: ['./comment.component.scss']
 })
+
 export class CommentComponent implements OnInit {
   @Input() id: string;
   post : Post;
   imageUrl: string;
+  isLoggedIn: boolean;
 
-  constructor(private postsService: PostsService) { }
+  constructor(
+    private postsService: PostsService,
+    private authService: AuthenticationService
+    ) { }
 
   ngOnInit() {
+
+    this.authService.getLoggedIn.subscribe(LoggedIn => {
+      this.isLoggedIn = LoggedIn == true;
+    });
 
     // read post from database
     this.postsService.getPost(this.id).subscribe(data => {
