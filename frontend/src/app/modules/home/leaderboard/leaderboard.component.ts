f62de680b7c27e1bfe7127bdc58eeeb747eaaf9c
“Alex” <AlexanderDesmond848898@gmail.com>
2019-10-05 15:44:51 +1000

    Add logic to populate leaderboar with users with most posts.

--- frontend/src/app/modules/home/leaderboard/leaderboard.component.ts
+++ frontend/src/app/modules/home/leaderboard/leaderboard.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from "@angular/core";
 
 import { UsersService } from "../../../services/users.service";
 import { User } from "src/app/user.interface";
+import { Post } from "src/app/post.interface";
+import { PostsService } from "src/app/services/posts.service";
 
 @Component({
   selector: "app-leaderboard",
@@ -10,8 +12,16 @@ import { User } from "src/app/user.interface";
 })
 export class LeaderboardComponent implements OnInit {
   users: User[];
+  posts: Post[];
+  uniqueUsers: {
+    uniqueUser: string;
+    postCount: number;
+  }[];
 
-  constructor(private userService: UsersService) {}
+  constructor(
+    private userService: UsersService,
+    private postService: PostsService
+  ) {}
 
   ngOnInit() {
     this.getUsers();
@@ -19,5 +29,26 @@ export class LeaderboardComponent implements OnInit {
 
   getUsers() {
     this.userService.getUsers().subscribe(data => (this.users = data));
+  }
+
+  getPosts() {
+    this.postService.getPosts().subscribe(data => (this.posts = data));
+  }
+
+  populateLeaderboard() {
+    this.getPosts();
+
+    for (let i = 0; i < this.users.length; i++) {
+      for (let j = 0; j < this.posts.length; j++) {
+        let uniqueUser = this.users[i].username.toString();
+        let postCount = 0;
+
+        if (this.posts[j].author === this.users[i].username) {
+          postCount++;
+        }
+
+        this.uniqueUsers.push({ uniqueUser, postCount });
+      }
+    }
   }
 }
