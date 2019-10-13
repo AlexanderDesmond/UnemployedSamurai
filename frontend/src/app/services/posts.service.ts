07b0ff899ba34a40a35ceb2477b663c584eabe44
“Alex” <AlexanderDesmond848898@gmail.com>
2019-10-14 10:44:50 +1100

    Add documentation to user and post services.

--- frontend/src/app/services/posts.service.ts
+++ frontend/src/app/services/posts.service.ts
@@ -11,40 +11,49 @@ import { Post } from "../post.interface";
 export class PostsService {
   constructor(private http: HttpClient) {}
 
+  // Uploads new post to the database.
   createPost(formData: FormData): Observable<Post> {
     return this.http.post<Post>("/api/post/new", formData);
   }
 
+  // Adds new comment to a post.
   addComment(formData: FormData, postid: String) {
     return this.http.post("/api/post/comment/" + postid, formData);
   }
 
+  // Returns the user's reaction to a post.
   getReaction(postid: String, username: String) {
     return this.http.post("/api/post/react/get/" + postid, {
       username: username
     });
   }
 
+  // Adds a reaction.
   addReaction(reaction: String, postid: String) {
     return this.http.post("/api/post/react/" + postid, { reaction: reaction });
   }
 
+  // Removes a reaction.
   removeReaction(postid: String) {
     return this.http.delete("/api/post/react/" + postid);
   }
 
+  // Return lost of posts.
   getPosts(page: number) {
     return this.http.get<Post[]>("/api/posts/all/" + page);
   }
 
+  // Return list of posts sorted by number of reacts.
   getPostsTrending(page: number) {
     return this.http.get<Post[]>("/api/posts/trending/" + page);
   }
 
+  // Return post.
   getPost(id: string): Observable<Post> {
     return this.http.get<Post>("/api/post/" + id);
   }
 
+  // Delete post.
   deletePost(id: string) {
     return this.http.delete("/api/post/" + id);
   }
--- frontend/src/app/services/users.service.ts
+++ frontend/src/app/services/users.service.ts
@@ -10,19 +10,22 @@ import { User } from "../user.interface";
 export class UsersService {
   constructor(private http: HttpClient) {}
 
-  // This is implemented in the frontend!
+  // Sends user login information to the backend.
   loginUser(username: string, password: string) {
     return this.http.post("/api/login/", { username, password });
   }
 
+  // Uploads new user to the database.
   registerUser(username: string, email: string, password: string) {
     return this.http.post("/api/users/", { username, email, password });
   }
 
+  // Returns a list of users.
   getUsers(): Observable<User[]> {
     return this.http.get<User[]>("/api/users/");
   }
 
+  // Returns a user based on username.
   getUser(username: string): Observable<User> {
     return this.http.get<User>("/api/users/" + username);
   }
